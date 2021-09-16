d5602602d9f30b7f76a9d5aff19ab82feb2d06eb
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-09-16 22:02:32 +0200

    first build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
-import * as OrbitControls from 'three-orbitcontrols';
-import { light2, light3, light4 } from './direct_light.js';
-import { mixer, action, gltfLoader2 } from './loader';
+//import * as OrbitControls from 'three-orbitcontrols';
+import { light2, light3, light4 } from './directlights.js';
+//import { mixer, action, gltfLoader2 } from './loader';
 
 export const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
